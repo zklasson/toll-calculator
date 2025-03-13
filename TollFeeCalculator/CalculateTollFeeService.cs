7798e94d44c48df368ca6f3840c0761a682733fc
Z <N/A>
2025-03-13 21:44:18 +0100

    Refactored method and variable names

--- TollFeeCalculator/CalculateTollFeeService.cs
+++ TollFeeCalculator/CalculateTollFeeService.cs
@@ -2,20 +2,23 @@
 
 public interface ICalculateTollFeeService
 {
-    decimal CalculateTotalTollFeeForDate(VehicleType vehicleType, DateOnly date, TimeOnly[] passageTimes);
+    TollFeeCalculationResult CalculateTotalTollFee(VehicleType vehicleType, DateOnly date, TimeOnly[] passageTimes);
 }
 
 public class CalculateTollFeeService(ITollFeePolicy policy) : ICalculateTollFeeService
 {
     private readonly ITollFeePolicy _policy = policy ?? throw new ArgumentNullException(nameof(policy));
 
-    public decimal CalculateTotalTollFeeForDate(VehicleType vehicleType, DateOnly date, TimeOnly[] passageTimes)
+    public TollFeeCalculationResult CalculateTotalTollFee(VehicleType vehicleType, DateOnly date, TimeOnly[] passageTimes)
     {
         if (passageTimes == null)
             throw new ArgumentNullException(nameof(passageTimes));
 
+        var currency = _policy.Currency;
+        var dailyMaxFee = _policy.DailyMaxFee;
+
         if (passageTimes.Length == 0 || _policy.IsTollFreeDate(date) || _policy.IsTollFreeVehicleType(vehicleType))
-            return 0;
+            return new TollFeeCalculationResult(date, 0, currency);
 
         decimal totalFee = 0;
         foreach (var passageTime in passageTimes.GroupBy(time => time.Hour))
@@ -27,21 +30,18 @@ public class CalculateTollFeeService(ITollFeePolicy policy) : ICalculateTollFeeS
             }
 
             totalFee += passageTime.Max(GetTollFeeForPass);
-
-            if (totalFee > _policy.DailyMaxFee)
-                return _policy.DailyMaxFee;
         }
 
-        return totalFee;
+        return totalFee > dailyMaxFee ? new TollFeeCalculationResult(date, dailyMaxFee, currency) : new TollFeeCalculationResult(date, totalFee, currency);
     }
 
     private decimal GetTollFeeForPass(TimeOnly passageTime)
     {
         var rate = _policy.TollFeeRates.FirstOrDefault(rate => rate.IsWithinTimeInterval(passageTime));
 
-        if (rate?.TollFee == null)
+        if (rate == null)
             throw new MissingTollFeeRateException(passageTime);
 
-        return rate.TollFee.Fee;
+        return rate.Fee;
     }
 }
